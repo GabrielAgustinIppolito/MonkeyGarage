d1b6aaa3eaedf5cd73b757ca8f8c97edfe3b964e
GabrielAgustinIppolito <gabrielpplt@gmail.com>
2023-07-11 16:50:08 +0200

    Initial commit

---
+++ src/main/java/it/discordcgm/monkeygarage/MonkeyGarageApplication.java
@@ -0,0 +1,13 @@
+package it.discordcgm.monkeygarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyGarageApplication.class, args);
+    }
+
+}
